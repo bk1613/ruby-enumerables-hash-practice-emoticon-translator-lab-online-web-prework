1aeecb311bc19cad918dc1c664f29dc020fde8e6
Brian Kao <bk1613@nyu.edu>
2019-09-08 02:08:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon(file = 'lib/emoticons.yml', emoticons)
   
 end
 
-def get_english_meaning
+def get_english_meaning(file = 'lib/emoticons.yml', emoticons)
   # code goes here
   west_emo = load_library
   
