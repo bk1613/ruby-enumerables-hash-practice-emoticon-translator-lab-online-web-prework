d14da7b5712d0e286944e4ad381502b22c891c5d
Brian Kao <bk1613@nyu.edu>
2019-09-08 01:47:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,27 @@
 # require modules here
+require "yaml"
 
-def load_library
+def load_library(file = 'lib/emoticons.yml')
   # code goes here
+  emo_library = {"get_meaning" = [], "get_emoticon" = []}
+  emos = YAML.load_file(file)
+  emos.each do |meaning, array|
+    emo_library["get_meaning"][array[0]] = meaning
+    emo_library["get_emoticon"][array[0]] = array[1]
+  end
+  emo_library
 end
 
 def get_japanese_emoticon
   # code goes here
+  jap_emo = load_library
+  
 end
 
 def get_english_meaning
   # code goes here
+  west_emo = load_library
+  
 end
+
+load_library
